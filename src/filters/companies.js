a55d895fe249008ce5adba6526579169175fc067
Saba <saba.sadegh67@gmail.com>
2023-01-05 17:49:49 +0330

    filtering by company name

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -9,6 +9,23 @@ const setupCompanies = (store) => {
       return `<button class="company-btn">${company}</button>`;
     })
     .join("");
+  companiesDOM.addEventListener("click", function (e) {
+    const element = e.target;
+    if (element.classList.contains("company-btn")) {
+      let newStore = [];
+      if (element.textContent === "all") {
+        newStore = [...store];
+      } else {
+        newStore = store.filter(
+          (product) => product.company === element.textContent
+        );
+        // newStore = store.filter(
+        //   (product) => product.company === e.target.textContent
+        // );
+      }
+      display(newStore, getElement(".products-container"));
+    }
+  });
 };
 
 export default setupCompanies;
