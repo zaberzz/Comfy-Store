1290afec018a73297a2d92b2ec79cd601cfe3157
Saba <saba.sadegh67@gmail.com>
2023-01-06 13:36:55 +0330

    creating a product page for each product

--- src/pages/product.js
+++ src/pages/product.js
@@ -28,6 +28,23 @@ window.addEventListener("DOMContentLoaded", async function () {
     const response = await fetch(`${singleProductUrl}${urlID}`);
     if (response.status >= 200 && response.status <= 299) {
       const product = await response.json();
+      const { id, fields } = product;
+      productID = id;
+      const { name, company, price, colors, description } = fields;
+      const image = fields.image[0].thumbnails.large.url;
+      document.title = `${name.toUpperCase()} | Comfy`;
+      pageTitleDOM.textContent = `Home/${name}`;
+      imgDOM.src = image;
+      titleDOM.textContent = name;
+      companyDOM.textContent = `by ${company}`;
+      priceDOM.textContent = formatPrice(price);
+      descDOM.textContent = description;
+      colors.forEach((color) => {
+        const span = document.createElement("span");
+        span.classList.add("product-color");
+        span.style.background = `${color}`;
+        colorsDOM.appendChild(span);
+      });
     } else {
       console.log(response.status, response.statusText);
       centerDOM.innerHTML = `<div>
@@ -40,4 +57,8 @@ window.addEventListener("DOMContentLoaded", async function () {
   }
 
   loading.style.display = "none";
+});
+
+cartBtn.addEventListener("click", function () {
+  addToCart(productID);
 });
