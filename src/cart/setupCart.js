743aff420f82ee762406cf1b49c72039ae8ab15d
Saba <saba.sadegh67@gmail.com>
2023-01-06 16:59:03 +0330

    define variables

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -9,6 +9,11 @@ import { openCart } from "./toggleCart.js";
 import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
 
 export const addToCart = (id) => {
   console.log(id);
